db85991e3dd96b648da5794bae0d7f3d4ebe5612
CollsalV <hartiavibhav15@gmail.com>
2021-08-25 14:50:51 +0530

    update

---
+++ IF.py
@@ -0,0 +1,23 @@
+#DO NOT RUN THE CODE
+import pygame as pg
+import random
+a = 0
+
+pg.init()
+
+#setting up the pygame window
+clock=pg.time.Clock() 
+screen = pg.display.set_mode([500, 500]) #screen size
+square = pg.Surface((20, 20)) #idk lol
+square.fill((255, 255, 255)) #screen color, RGB
+rect = square.get_rect() #creates a square
+rect.fill((255,255,255)) 
+rect.x == 250
+rect.y == 250
+
+#this is the loop we'll code inside
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+    
