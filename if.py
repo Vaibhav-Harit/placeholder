4ab83d154c325f87f17d36d67e1787b772f454ab
CollsalV <hartiavibhav15@gmail.com>
2021-08-26 15:25:44 +0530

    update

---
+++ if.py
@@ -0,0 +1,26 @@
+import pygame as pg
+import random
+a = 0
+
+pg.init()
+
+#setting up the pygame window
+clock=pg.time.Clock() 
+screen = pg.display.set_mode([500, 500]) #screen size
+square = pg.Surface((20, 20)) #creates a surface with a given width and height
+square.fill((255, 255, 255)) #screen color, RGB
+rect = square.get_rect() #creates a square
+
+
+#this is the loop we'll code inside
+while True:
+    clock.tick(60)#DO NOT REMOVE THIS 
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit() #quits the pygame window when we click on X 
+    rect.x = 240
+    rect.y = 240
+    screen.fill((0, 0, 0))
+    screen.blit(square, rect)
+    pg.display.flip()
+    
