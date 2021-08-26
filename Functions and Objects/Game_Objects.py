0a6625f6e5180228f6de143e441e3ea9a9835062
FrostLily45 <89291799+FrostLily45@users.noreply.github.com>
2021-08-26 19:22:55 +0800

    update

---
+++ Functions and Objects/Game_Objects.py
@@ -0,0 +1,28 @@
+class Player(object):
+    #initialization, will automatically run when you do a new instance
+    def __init__(self, pwr, spd, defense, sprite, position, direction):
+        self.power = pwr
+        self.speed = spd
+        self.defense = defense
+        self.sprite = sprite
+        self.position = position
+        self.direction = direction
+
+    def attack():
+        pass
+
+    def sprint():
+        pass
+
+    def move():
+        pass
+
+    def block():
+        pass
+
+    def pick_up():
+        pass
+
+    def activate_skill():
+        pass
+    
