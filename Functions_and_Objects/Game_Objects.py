c0fc593593a9306230078d33a3c9592427d11a00
CollsalV <hartiavibhav15@gmail.com>
2021-08-27 17:26:08 +0530

    update

---
+++ Functions_and_Objects/Game_Objects.py
@@ -0,0 +1,39 @@
+import pygame as pg 
+import math
+import assets
+
+sprite_1 = pg.image.load("dungeon_tiles.png")
+
+
+class Player(object):
+    #initialization, will automatically run when you do a new instance
+    def __init__(self, pwr, spd, defense, sprite, position, direction):
+        self.power = pwr
+        self.speed = spd
+        self.defense = defense
+        self.sprite = sprite
+        self.position = position
+        self.direction = direction
+
+    def attack():
+        pass
+
+    def sprint():
+        pass
+
+    def move():
+        pass
+
+    def block():
+        pass
+
+    def pick_up():
+        pass
+
+    def activate_skill():
+        pass
+
+    def draw()
+        pg.blit(self.sprite, self.position )
+    
+    charac_1 = Player(10, 10, 10, sprite_1, (250,250), 0)
