1c8f380dc3c02c8fd7523c63b472d7c9b5525a61
FrostLily45 <frostlily45@gmail.com>
2021-09-07 22:17:41 +0800

    made the item_list keys be equal to the Item ID

--- functions/Inventory_Class.py
+++ functions/Inventory_Class.py
@@ -15,13 +15,14 @@ class Item:
 def initialize_items(items_dict):   #a function to initialize all items in a processed dictionary
     item_list = {}
     for item in items_dict:
-        item_list[item['Item Name']] = Item(item['Item Name'], item['Item Type'], item['Item Rarity'], item['Item ID'], item['Sprite'], item['Class Type'], item['Power'], item['Speed'])
+        item_list[item['Item ID']] = Item(item['Item Name'], item['Item Type'], item['Item Rarity'], item['Item ID'], item['Sprite'], item['Class Type'], item['Power'], item['Speed'])
     return item_list
 
 #initializing items through functions
 items_df = pandas.read_excel('assets\item_list.xlsx', sheet_name = 'ItemList')
 item_dictionary = items_df.to_dict('records')
 item_list = initialize_items(item_dictionary)
+print(item_list)
 
 #----------INVENTORY SYSTEM----------#
 class Inventory:
