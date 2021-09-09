a23dae810ddd3c7da985f5835421dd4773f3a4d9
CollsalV <hartiavibhav15@gmail.com>
2021-09-09 20:28:01 +0530

    made some changes to the chest class, need to test the placeholder sprite blit code

--- functions/Chest_objects.py
+++ functions/Chest_objects.py
@@ -1,21 +1,40 @@
 import pygame as pg
 import math
+import random
+
+clock=pg.time.Clock()
+sc_width = 1300
+sc_height = 700
+screen = pg.display.set_mode([sc_width, sc_height])
 
 class chest:
     def __init__(self, pos, sprite):
         self.img = sprite
-        self.initial_position = pos
+        self.pos = pos
         self.x = pos[0]
         self.y = pos[1]
 
     def spawn(self, screen: pg.display):
-        screen.blit(self.sprite, (self.x, self.y))
+        screen.blit(self.img, (self.x, self.y))
 
     def decide_pos(self):
-        decided_pos = []
+        pass
+        #decided_pos = []
 
-        random_x = random.randint(-1 * (range), range)
-        random_y = random.randint(-1 * (range), range)
-        set_destination = ((self.x + random_x), (self.y + random_y))
+        #random_x = random.randint(100,500)
+        #random_y = random.randint(100,500)
+        #set_destination = ((self.x + random_x), (self.y + random_y))
         
+chest1 = chest((100,500), pg.image.load('assets//interface//play.png'))
 
+running = True
+while running:
+    clock.tick(60)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    chest1.spawn(screen)
+
+pg.display.flip()
