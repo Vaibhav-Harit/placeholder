01de35ea0d638fae65dc4bb257627d322b30d1a8
FrostLily45 <frostlily45@gmail.com>
2021-09-16 00:10:35 +0800

    made a separate folder for spreadsheets and added a description attribute to item class

--- functions/Inventory_Class.py
+++ functions/Inventory_Class.py
@@ -2,7 +2,7 @@ import pandas
 
 #----------ITEMS CLASS----------#
 class Item:
-    def __init__(self, name, type, rarity, ID, sprite = None, item_class1 = None, item_class2 = None, power = None, speed = None):
+    def __init__(self, name, type, rarity, ID, sprite = None, description = None, item_class1 = None, item_class2 = None, power = None, speed = None):
         self.name = name
         self.type = type
         self.rarity = rarity
@@ -12,15 +12,16 @@ class Item:
         self.second_class = item_class2
         self.power = power
         self.speed = speed
+        self.description = description
 
 def initialize_items(items_dict):   #a function to initialize all items in a processed dictionary
     item_list = {}
     for item in items_dict:
-        item_list[item['Item ID']] = Item(item['Item Name'], item['Item Type'], item['Item Class Set'], item['Item ID'], item['Sprite'], item['Class Type 1'], item['Class Type 2'], item['Power'], item['Speed'])
+        item_list[item['Item ID']] = Item(item['Item Name'], item['Item Type'], item['Item Class Set'], item['Item ID'], item['Sprite'], item['Description'], item['Class Type 1'], item['Class Type 2'], item['Power'], item['Speed'])
     return item_list
 
 #initializing items through functions
-items_df = pandas.read_excel('assets\item_list.xlsx', sheet_name = 'ItemList')
+items_df = pandas.read_excel('assets\lists\item_list.xlsx', sheet_name = 'ItemList')
 item_dictionary = items_df.to_dict('records')
 item_list = initialize_items(item_dictionary)
 
